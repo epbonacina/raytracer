11706f0216711af109de151e66bab22a1497bd96
Enzo Pedro Bonacina <epbonacina@gmail.com>
2024-06-17 15:07:40 -0300

    Starts applying gamma correction.

--- src/color.rs
+++ src/color.rs
@@ -4,9 +4,9 @@ pub type Color = Vec3;
 
 impl Color {
     pub fn print(&self) {
-        let r = self.x();
-        let g = self.y();
-        let b = self.z();
+        let r = Color::linear_to_gamma(self.x());
+        let g = Color::linear_to_gamma(self.y());
+        let b = Color::linear_to_gamma(self.z());
 
         let intensity = 0.0..0.999;
         let ru = MAX_COLOR as f64 * intensity.make_fit(r);
@@ -14,5 +14,12 @@ impl Color {
         let bu = MAX_COLOR as f64 * intensity.make_fit(b);
 
         println!("{ru} {gu} {bu}");
+    }
+
+    fn linear_to_gamma(linear_component: f64) -> f64 {
+        if linear_component > 0.0 {
+            return linear_component.sqrt();
+        }
+        0.0
     }
 }
